93f5008c955799b1b9ce96cf0a4531dc361f54bf
MidWest005 <mlwest5@gmail.com>
2024-03-19 00:19:44 -0500

    test add file

--- March 2024 Code/March 2024 Code/Program.cs
+++ March 2024 Code/March 2024 Code/Program.cs
@@ -7,6 +7,7 @@ namespace March_2024_Code
         {
             Console.WriteLine("hello worldy");
             Console.WriteLine("testing a text change and a delete");
+            Console.WriteLine("Testing adding one file at a time");
         }
 
         /* 3/18/2024
